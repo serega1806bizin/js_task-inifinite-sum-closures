b488f83361e0c621e0f91b418f1e196863b97fa3
Serhii Bizin <seregabizin@icloud.com>
2024-10-12 20:33:04 +0300

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0; // Змінна для збереження поточної суми
+
+  function adder(...args) {
+    if (args.length === 0) {
+      const result = sum;
+
+      sum = 0; // Очищаємо суму після повернення результату
+
+      return result;
+    } else {
+      sum += args.reduce((acc, num) => acc + num, 0); // Додаємо всі числа
+
+      return adder; // Повертаємо саму функцію для подальших викликів
+    }
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
